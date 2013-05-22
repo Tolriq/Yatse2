a72f56c95845484b38f5a99c5e95b14dd2fb2c82
Tolriq <Tolriq+dev@gmail.com>
2013-05-22 14:03:02 +0200

    Correct crash due to Yahoo Weather api change. Bump build to 146

--- Yatse2/Properties/AssemblyInfo.cs
+++ Yatse2/Properties/AssemblyInfo.cs
@@ -49,8 +49,8 @@ using System.Windows;
 //
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
-[assembly: AssemblyVersion("2.0.144")]
+[assembly: AssemblyVersion("2.0.146")]
 //[assembly: AssemblyVersion("2.0.0.101")]
 //[assembly: AssemblyFileVersion("2.0.0.101")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
-[assembly: AssemblyFileVersionAttribute("2.0.144")]
+[assembly: AssemblyFileVersionAttribute("2.0.146")]
